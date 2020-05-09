8af32dad10f3d1af8584bfa1f21cf616473e2136
Clément Rousseau <clement@agence-belle-epoque.fr>
2020-05-09 14:58:27 +0200

    Fix stream problem

--- server.js
+++ server.js
@@ -43,38 +43,37 @@ app.post('/download', async (req, res) => {
       if (err) throw err;
       ytdl(url, {
         format: 'mp4',
-      }).pipe(fs.createWriteStream(output(info.title)));
-    });
-
-    video.once('response', () => {
-      starttime = Date.now();
-    });
-    video.on('progress', (chunkLength, downloaded, total) => {
-      const percent = downloaded / total;
-      const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
-      const estimatedDownloadTime =
-        downloadedMinutes / percent - downloadedMinutes;
-      readline.cursorTo(process.stdout, 0);
-      process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
-      process.stdout.write(
-        `(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(
-          total /
-          1024 /
-          1024
-        ).toFixed(2)}MB)\n`
-      );
-      process.stdout.write(
-        `running for: ${downloadedMinutes.toFixed(2)}minutes`
-      );
-      process.stdout.write(
-        `, estimated time left: ${estimatedDownloadTime.toFixed(2)}minutes `
-      );
-      readline.moveCursor(process.stdout, 0, -1);
-    });
-    video.on('end', () => {
-      console.log('fin du dl');
-      process.stdout.write('\n\n');
-      res.status(200).send('some text');
+      })
+        .once('response', () => {
+          starttime = Date.now();
+        })
+        .on('progress', (chunkLength, downloaded, total) => {
+          const percent = downloaded / total;
+          const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
+          const estimatedDownloadTime =
+            downloadedMinutes / percent - downloadedMinutes;
+          readline.cursorTo(process.stdout, 0);
+          process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
+          process.stdout.write(
+            `(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(
+              total /
+              1024 /
+              1024
+            ).toFixed(2)}MB)\n`
+          );
+          process.stdout.write(
+            `running for: ${downloadedMinutes.toFixed(2)}minutes`
+          );
+          process.stdout.write(
+            `, estimated time left: ${estimatedDownloadTime.toFixed(2)}minutes `
+          );
+          readline.moveCursor(process.stdout, 0, -1);
+        })
+        .on('end', () => {
+          process.stdout.write('\n\n');
+          res.status(200).send('Dl end');
+        })
+        .pipe(fs.createWriteStream(output(info.title)));
     });
   }
 });
